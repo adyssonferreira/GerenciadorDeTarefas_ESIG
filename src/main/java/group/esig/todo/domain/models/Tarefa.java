22ee4ad5e926390b130e0925d341a97d559ebbf3
adysson.silva <adysson.silva@softplan.com.br>
2025-08-12 20:30:06 -0300

    Adiciona novos atributos e anotações à classe Tarefa, incluindo título e deadline

--- src/main/java/group/esig/todo/domain/models/Tarefa.java
+++ src/main/java/group/esig/todo/domain/models/Tarefa.java
@@ -2,20 +2,25 @@ package group.esig.todo.domain.models;
 
 
 import group.esig.todo.domain.enums.TarefaStatus;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
 import group.esig.todo.domain.enums.Prioridade;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Tarefa {
 
     private UUID id;
+    private String titulo;
     private String descricao;
     private Usuario responsavel;
     private Prioridade prioridade;
     private TarefaStatus status;
+    private Instant deadline;
 }

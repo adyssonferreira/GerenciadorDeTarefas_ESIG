627feed08a18672015213f74c8447f6e63dc5fce
adysson.silva <adysson.silva@softplan.com.br>
2025-08-12 20:35:27 -0300

    Remove construtor redundante da classe ResourceNotFoundException

--- src/main/java/group/esig/todo/infrastructure/web/exception/ResourceNotFoundException.java
+++ src/main/java/group/esig/todo/infrastructure/web/exception/ResourceNotFoundException.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
         super(String.format("%s não encontrado com %s: '%s'", resourceName, fieldName, fieldValue));
     }

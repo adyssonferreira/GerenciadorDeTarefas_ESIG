118ebb7ff7a9a70929ac341c454739dc823f95e5
adysson.silva <adysson.silva@softplan.com.br>
2025-08-15 17:46:02 -0300

    Adiciona redirecionamento da rota raiz para `/login` no frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {LoginPage} from './features/auth/pages/login-page/login-page';
 import {ListaTarefasPage} from './features/todo/pages/lista-tarefas-page/lista-tarefas-page';
 
 export const routes: Routes = [
-  {path: "login", component: LoginPage},
-  {path: "tarefas", component: ListaTarefasPage}
+    {path: "", redirectTo: "/login", pathMatch: "full"},
+    {path: "login", component: LoginPage},
+    {path: "tarefas", component: ListaTarefasPage}
 ];

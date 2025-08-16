a2950f42a318062d08245ee41151678b11c0a130
adysson.silva <adysson.silva@softplan.com.br>
2025-08-16 09:36:24 -0300

    Adiciona tratamento para validação de argumentos no `GlobalExceptionHandler`

--- backend/src/main/java/group/esig/todo/presentation/web/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/group/esig/todo/presentation/web/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package group.esig.todo.presentation.web.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -30,5 +31,23 @@ public class GlobalExceptionHandler {
         body.put("mensagem", ex.getMessage());
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationErrors(MethodArgumentNotValidException ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("mensagem", "Erro de validação dos dados de entrada");
+        body.put("erros", ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> {
+                    assert error.getDefaultMessage() != null;
+                    return Map.of(
+                            "campo", error.getField(),
+                            "mensagem", error.getDefaultMessage()
+                    );
+                })
+                .toList());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
